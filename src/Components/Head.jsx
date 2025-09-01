24197a373034d4d4a2a352aa8b8cf47195d18333
ashiredev <219289962+ashiredev@users.noreply.github.com>
2025-09-01 19:23:08 +0200

    Fix favicon link

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -4,7 +4,7 @@ function Head() {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/x-icon" href="src/assets/hoppe.jpg"></link>
+        <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/AshireSoftware/Ashirium/refs/heads/hoppe/hoppe.jpg"></link>
         <title>AshireOS</title>
       </head>
     </>
